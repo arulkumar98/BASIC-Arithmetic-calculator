42fb9fef236a13033592c2a0ce37d668db62d971
Arul Kumar <56965430+arulkumar98@users.noreply.github.com>
2022-09-07 17:47:11 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,35 +1,44 @@
 import java.util.Scanner;
 
-class Calculator {
+public class Calculator {
     public static void main(String[] args) {
-        // here I am going to make a calculator using Nested if-else
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the Number one :");
-        int num1 = in.nextInt();
-        System.out.println("Current version Supported operator +,-,*,/,Reminder % ");
-        System.out.print("Enter the Math operator :");
-        char op = in.next().trim().charAt(0);
-        System.out.print("Enter the Number two :");
-        int num2 = in.nextInt();
+        // Take input from user till user does not press X or x
         int ans = 0;
+        while (true) {
+            // take the operator as input
+            System.out.print("Enter the operator: ");
+            char op = in.next().trim().charAt(0);
 
-        if (op == '+'){
-           ans = num1+num2;
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                // input two numbers
+                System.out.print("Enter two numbers: ");
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
 
-         } else if (op == '-') {
-            ans = num1-num2;
-
-            }else if (op == '*') {
-              ans = num1 * num2;
-
-             }else if (op == '/') {
-               ans = num1 / num2;
-
-               }else if ( op == '%') {
-                  ans = num1 % num2;
-                 }else System.out.println("Sorry our current version app not support this feature");
-
-        System.out.println("Answer is " +ans);
-
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    }
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+            } else if (op == 'x' || op == 'X') {
+                break;
+            } else {
+                System.out.println("Invalid operation!!");
+            }
+            System.out.println(ans);
+        }
     }
 }
